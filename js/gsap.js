5214f1a7d2c9d3e112746f8d12d6f9b5ff7fd962
窓際あつし <98872282+joetsu-ichimiya@users.noreply.github.com>
2025-07-09 06:17:38 +0000

    わからん

--- js/gsap.js
+++ js/gsap.js
@@ -6,17 +6,17 @@
 //     opacity: 0,//透明度
 //     ease: Power4.out,//変化の細かい設定
 // });
-gsap.to(".a01", {
-    x: 200,//位置
+gsap.to(".s01", {
+    x: 200, //位置
     duration: 1.3,
-    repeat: -1,//繰り返し
+    repeat: -1, //繰り返し
     repeatDelay: 0.5,
 });
 
 gsap.to(".s02", {
     backgroundColor: "#abcfe0",
-    duration: 1.5,
-    repeat: -1,//繰り返し
+    duration: 1.3,
+    repeat: -1, //繰り返し
     repeatDelay: 0.5,
 <<<<<<< HEAD
 });
@@ -25,14 +25,73 @@ gsap.to(".s02", {
 gsap.to(".s03", {
     opacity: 0.3,
     duration: 1.3,
-    repeat: -1,//繰り返し
+    repeat: -1, //繰り返し
     repeatDelay: 0.5,
 });
-gsap.to(".s032", {
+gsap.to(".s04", {
     rotate: 50,
     duration: 1.3,
-    repeat: -1,//繰り返し
+    repeat: -1, //繰り返し
     repeatDelay: 0.5,
 });
 
+<<<<<<< HEAD
 >>>>>>> 7a5b2bc (0704)
+=======
+gsap.to(".s05", {
+    border: "#0000FF",
+    duration: 1.3,
+    repeat: -1, //繰り返し
+    repeatDelay: 0.5,
+});
+// sec02
+gsap.to(".s06", {
+    x: 200, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+    repeatDelay: 0.5,
+});
+gsap.to(".s07", {
+    x: 200, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+    repeatDelay: 1.5,
+    x: 200,
+});
+gsap.to(".s08", {
+    x: 200, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+    paused: true,
+    x: 200, // アニメーションを一時停止
+});
+gsap.to(".s09", {
+    x: 200, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+    overwrite: true, // 上書き
+});
+gsap.to(".s10", {
+    x: 200, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+});
+gsap.to(".s11", {
+    x: 200, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+    repeatDelay: 2,
+});
+gsap.to(".s12", {
+    x: 200, //位置
+    duration: 1.3,
+    repeatDelay: 0.1, //遅延
+    repeat: -1, //繰り返し
+    yoyo: true,
+});
+gsap.to(".s13", {
+    x: 200, //位置
+    y: 20, //位置
+    duration: 1.3,
+    repeat: -1, //繰り返し
+});
